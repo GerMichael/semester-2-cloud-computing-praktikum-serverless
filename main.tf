fbbaa56333c65c02b2c798504688d8fede09b6da
Michael Gerischer <mi.gerischer@gmail.com>
2024-06-06 17:55:26 +0200

    add lambda

--- main.tf
+++ main.tf
@@ -17,6 +17,7 @@ resource "random_pet" "lambda_bucket_name" {
   length = 4
 }
 
+# Create bucket
 resource "aws_s3_bucket" "lambda_bucket" {
   bucket = "michael-${random_pet.lambda_bucket_name.id}"
 }
@@ -35,6 +36,7 @@ resource "aws_s3_bucket_acl" "lambda_bucket" {
   acl    = "private"
 }
 
+# package zip
 data "archive_file" "lambda_hello_world" {
   type = "zip"
   source_dir  = "${path.module}/hello-world"
@@ -44,6 +46,56 @@ data "archive_file" "lambda_hello_world" {
 resource "aws_s3_object" "lambda_hello_world" {
   bucket = aws_s3_bucket.lambda_bucket.id
   key    = "hello-world.zip"
+  # put zip to bucket
   source = data.archive_file.lambda_hello_world.output_path
   etag = filemd5(data.archive_file.lambda_hello_world.output_path)
 }
+
+resource "aws_lambda_function" "hello_world" {
+  function_name = "HelloWorld"
+
+# listen on bucket events
+  s3_bucket = aws_s3_bucket.lambda_bucket.id
+  s3_key    = aws_s3_object.lambda_hello_world.key
+
+  runtime = "nodejs20.x"
+  handler = "hello.handler"
+
+# if hash changes, it updates code
+  source_code_hash = data.archive_file.lambda_hello_world.output_base64sha256
+
+  role = aws_iam_role.lambda_exec.arn
+}
+
+resource "aws_cloudwatch_log_group" "hello_world" {
+  # create cloudwatch log storage which stores logs of ${function_name}
+  name = "/aws/lambda/${aws_lambda_function.hello_world.function_name}"
+
+  retention_in_days = 30
+}
+
+resource "aws_iam_role" "lambda_exec" {
+  name = "serverless_lambda"
+
+  # lambda is allowed to read s3 bucket
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Sid    = ""
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+      }
+    ]
+  })
+}
+
+# attach policy to IAM role so lambda can write into logs
+# policy is maintained by AWS
+resource "aws_iam_role_policy_attachment" "lambda_policy" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
